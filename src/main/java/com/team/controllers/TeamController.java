40143f2e872427363158d6e5ba005f68500a898c
Camara <elyroot94@gmail.com>
2025-02-03 08:55:01 +0100

    swager docs

--- src/main/java/com/team/controllers/TeamController.java
+++ src/main/java/com/team/controllers/TeamController.java
@@ -4,6 +4,10 @@ import com.team.dto.TeamRequestDto;
 import com.team.dto.TeamResponseDto;
 import com.team.models.Tri;
 import com.team.services.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/teams")
+@Tag(name = "Team Api", description = "Gestion de l'equipe ")
 public class TeamController {
 
 
@@ -48,7 +53,10 @@ public class TeamController {
      *         lors de la création de l'équipe.
      */
     @PostMapping
-    public ResponseEntity<TeamResponseDto> CreateTeam(@Valid @RequestBody TeamRequestDto team) {
+    @Operation(summary = "Ajout d'une equipe", description = "Retourne l'equipe creer")
+    @ApiResponse(responseCode = "201", description = "equipe creé")
+    @ApiResponse(responseCode = "404", description = "si les jouers n'existe pans dans la base")
+    public ResponseEntity<TeamResponseDto> CreateTeam(@Parameter(description = "Détails de l'equipe (name,accronym,budget)", required = true) @Valid @RequestBody TeamRequestDto team) {
         return new ResponseEntity<>(this.teamService.Create(team), HttpStatus.CREATED);
     }
 
@@ -63,10 +71,13 @@ public class TeamController {
      */
 
     @GetMapping
-    public ResponseEntity<Page<TeamResponseDto>> ListTeams(@RequestParam(defaultValue = "name") Tri sort,
-                                                           @RequestParam(defaultValue = "ASC") String direction,
-                                                           @RequestParam(defaultValue = "0") int pageNo,
-                                                           @RequestParam(defaultValue = "10") int pageSize) {
+    @Operation(summary = "liste d'equipes", description = "renvoie liste d'equipes avec ou sans joueurs")
+    @ApiResponse(responseCode = "201", description = "Joueur créé")
+    @ApiResponse(responseCode = "200")
+    public ResponseEntity<Page<TeamResponseDto>> ListTeams( @Parameter(description = "tri") @RequestParam(defaultValue = "name") Tri sort,
+                                                            @Parameter(description = "direction tri")  @RequestParam(defaultValue = "ASC") String direction,
+                                                            @Parameter(description = "numero de page") @RequestParam(defaultValue = "0") int pageNo,
+                                                            @Parameter(description = "taille")  @RequestParam(defaultValue = "10") int pageSize) {
         return new ResponseEntity<>(this.teamService.listOFTeams(pageNo, pageSize, sort, direction), HttpStatus.OK);
 
 
